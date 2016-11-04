98b663e3aa97ed865ee65877cc80231c87b33271
Jacob Mao <jacobmao0416@outlook.com>
2016-11-04 22:55:58 +0800

    for commited

--- Sorting.swift
+++ Sorting.swift
@@ -1,6 +1,14 @@
 import Foundation
 
 struct Sorting {
+    static func isSorted<T: Comparable>(items: [T]) -> Bool {
+        let sortedItems = items.sorted { (item1, item2) -> Bool in
+            return item1 < item2
+        }
+        
+        return sortedItems == items
+    }
+    
     func selectionSort<T: Comparable>(items: [T]) -> [T] {
         if items.count <= 1 {
             return items
@@ -74,13 +82,60 @@ struct Sorting {
 
         return tempArray
     }
+    
+    func mergeSort<T: Comparable>(items: inout [T]) {
+        func mergeProcedure(l: Int, m: Int, r: Int) {
+            if items[m] <= items[m+1] {
+                return
+            }
+            
+            var leftIndex = l
+            var rightIndex = m + 1
+            
+            let subItems = items[l..<r+1]
+            
+            func setLeftItemProcedure(_ i: Int) {
+                items[i] = subItems[leftIndex]
+                leftIndex += 1
+            }
+            
+            func setRightItemProcedure(_ i: Int) {
+                items[i] = subItems[rightIndex]
+                rightIndex += 1
+            }
+            
+            for i in l...r {
+                if leftIndex > m {
+                    setRightItemProcedure(i)
+                    continue
+                }
+                
+                if rightIndex > r {
+                    setLeftItemProcedure(i)
+                    continue
+                }
+                
+                if subItems[leftIndex] <= subItems[rightIndex] {
+                    setLeftItemProcedure(i)
+                } else {
+                    setRightItemProcedure(i)
+                }
+            }
+        }
+        
+        func sort(lo: Int, hi: Int) {
+            if hi <= lo {
+                return
+            }
+            
+            let mid = lo + (hi - lo) / 2
+            sort(lo: lo, hi: mid)
+            sort(lo: mid+1, hi: hi)
+            mergeProcedure(l: lo, m: mid, r: hi)
+        }
+        
+        sort(lo: 0, hi: items.endIndex - 1)
+    }
 
     // MARK: Private
 }
-
-let s = Sorting()
-
-let a = [3,6,8,4,24,6,8,7,4,3,8,7]
-let b = s.shellSort(items: a)
-
-print(b)
