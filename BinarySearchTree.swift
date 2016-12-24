5b3f5a13528fbcb9f4024bb7fa8be7841c96e3ce
Jacob Mao <jacobmao0416@outlook.com>
2016-12-24 11:46:48 +0800

    improved binary search tree.

--- BinarySearchTree.swift
+++ BinarySearchTree.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 private enum ThroughDownType {
-        case min
-        case max
+    case min
+    case max
 }
 
 class BinarySearchTree<T: Comparable>: ExpressibleByArrayLiteral {
@@ -15,11 +15,11 @@ class BinarySearchTree<T: Comparable>: ExpressibleByArrayLiteral {
     }
 
     var minValue: T? {
-        return self.throughDown(root: self.root, type: .min)
+        return self.throughDown(root: self.root, type: .min)?.value
     }
     
     var maxValue: T? {
-        return self.throughDown(root: self.root, type: .max)
+        return self.throughDown(root: self.root, type: .max)?.value
     }
 
     required init(arrayLiteral: T...) {
@@ -35,7 +35,7 @@ class BinarySearchTree<T: Comparable>: ExpressibleByArrayLiteral {
         var currentNode = self.root
         while let currentingNode = currentNode {
             selectedNode = currentNode
-            
+                
             if newNode < currentingNode {
                 currentNode = currentingNode.leftChild
             } else {
@@ -56,24 +56,43 @@ class BinarySearchTree<T: Comparable>: ExpressibleByArrayLiteral {
             selectingNode.rightChild = newNode
         }
     }
+        
+    func remove(_ value: T) {
+        guard let targetNode = self.search(value) else {
+            return
+        }
+            
+        if targetNode.leftChild == nil {
+            self.transplant(source: targetNode.rightChild, dest: targetNode)
+            return
+        }
+        
+        if targetNode.rightChild == nil {
+            self.transplant(source: targetNode.leftChild, dest: targetNode)
+            return
+        }
+        
+        guard let successorN = self.successorNode(after: targetNode) else {
+            return
+        }
+        
+        guard let successorNP = successorN.parent else {
+            return
+        }
+        
+        if successorNP != targetNode {
+            self.transplant(source: successorN.rightChild, dest: successorN)
+            successorN.rightChild = targetNode.rightChild
+            targetNode.rightChild?.parent = successorN
+        }
+        
+        self.transplant(source: successorN, dest: targetNode)
+        successorN.leftChild = targetNode.leftChild
+        successorN.leftChild?.parent = successorN
+    }
 
     func contains(_ value: T) -> Bool {
-        var selectingNode = self.root
-        var ret = false
-        while let selectedNode = selectingNode {
-            if selectedNode.value == value {
-                ret = true
-                break
-            }
-
-            if selectedNode < value {
-                selectingNode = selectedNode.rightChild
-            } else {
-                selectingNode = selectedNode.leftChild
-            }
-        }
-
-        return ret
+        return self.search(value) != nil
     }
 
     func successor(after value: T) -> T? {
@@ -81,25 +100,32 @@ class BinarySearchTree<T: Comparable>: ExpressibleByArrayLiteral {
             return nil
         }
 
-        if node.rightChild != nil {
-            return self.throughDown(root: node, type: .min)
-        }
-
-        return nil
+        return self.successorNode(after: node)?.value
     }
-    
+        
     private func generateInorderWalk(node: Node?) -> [T] {
-        guard let currentNode = node else {
-            return []
+        var ret = [T]()
+        var currentNode = node
+        var s = Stack<Node>()
+        while !s.isEmpty || currentNode != nil {
+            if currentNode != nil {
+                s.push(currentNode!)
+                currentNode = currentNode?.leftChild
+            } else {
+                guard let popNode = s.pop() else {
+                    continue
+                }
+                
+                ret.append(popNode.value)
+                
+                currentNode = popNode.rightChild
+            }
         }
-
-        let leftValues = self.generateInorderWalk(node: currentNode.leftChild)
-        let rightValues = self.generateInorderWalk(node: currentNode.rightChild)
-
-        return leftValues + [currentNode.value] + rightValues
+        
+        return ret
     }
 
-    private func throughDown(root: Node?, type: ThroughDownType) -> T? {
+    private func throughDown(root: Node?, type: ThroughDownType) -> Node? {
         var currentNode = root
         switch type {
         case .min:
@@ -112,20 +138,62 @@ class BinarySearchTree<T: Comparable>: ExpressibleByArrayLiteral {
             }
         }
 
-        return currentNode?.value
+        return currentNode
     }
 
     private func search(_ value: T) -> Node? {
         var ret = self.root
         while let nodeValue = ret?.value,
               nodeValue != value {
-            if value < nodeValue {
-                ret = ret?.leftChild
-            } else {
-                ret = ret?.rightChild
+                if value < nodeValue {
+                    ret = ret?.leftChild
+                } else {
+                    ret = ret?.rightChild
+                }
             }
-        }
 
         return ret
+    }
+        
+    private func transplant(source: Node?, dest: Node?) {
+        defer {
+            if source != nil {
+                source!.parent = dest?.parent
+            }
+        }
+        
+        guard let destNode = dest else {
+            return
+        }
+        
+        guard let destP = destNode.parent else {
+            self.root = source
+            return
+        }
+        
+        if let leftNode = destP.leftChild, leftNode == destNode {
+            destP.leftChild = source
+            return
+        }
+        
+        destP.rightChild = source
+    }
+        
+    private func successorNode(after node: Node?) -> Node? {
+        if node?.rightChild != nil {
+            return self.throughDown(root: node, type: .min)
+        }
+            
+        var currentNode: Node? = node
+        var parentNode = node?.parent
+        while let pNode = parentNode,
+              let cNode = currentNode,
+              let rNode = pNode.rightChild,
+              cNode == rNode {
+                currentNode = pNode
+                parentNode = pNode.parent
+            }
+
+        return parentNode
     }
 }
